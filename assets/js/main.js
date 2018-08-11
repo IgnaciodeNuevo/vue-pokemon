98feadb0c3ca5a2f85788df754510d4b12ad9871
IgnaciodeNuevo <ignaciodenuevo@gmail.com>
2018-08-11 20:06:16 +0200

    Added getUnicTypes function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,36 @@ const app = new Vue({
     data() {
         return {
             text: 'Vue Pokedex',
-            pokemons: {}
+            pokemons: [],
+            pokemonTypes: []
         }
     },
     created() {
         fetch('http://localhost:3000/pokemons')
             .then(response => response.json())
             .then(pokemons => {
-                    this.pokemons = pokemons
+                this.pokemons = pokemons
+                this.pokemonTypes = this.getPokemonUnicTypes(pokemons);
                     console.log(this.pokemons);
+                    console.log(this.getPokemonUnicTypes(pokemons));
                 }
             )
     },
     methods: {
         getImage(imgSrc) {
             return `assets/${imgSrc}`
+        },
+        getPokemonUnicTypes(pokemons) {
+            const types = pokemons
+                    .map(pokemon => pokemon.types)
+                    .reduce((types, acc) => acc.concat(types), []);
+
+            // See Set here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+            const unicTypes = Array.from(new Set(types));
+            return unicTypes;
+        },
+        filterTypes(pokemons, types) {
+
         }
     }
 })
