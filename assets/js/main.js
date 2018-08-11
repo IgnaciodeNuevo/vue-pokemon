2aed010d8149be77009998c96baa8233291b0c8a
IgnaciodeNuevo <ignaciodenuevo@gmail.com>
2018-08-11 18:57:21 +0200

    Listed pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,17 @@ const app = new Vue({
     el: '#app',
     data() {
         return {
-            text: 'Vue is alive!'
+            text: 'Vue Pokedex',
+            pokemons: {}
         }
+    },
+    created() {
+        fetch('http://localhost:3000/pokemons')
+            .then(response => response.json())
+            .then(pokemons => {
+                    this.pokemons = pokemons
+                    console.log(this.pokemons);
+                }
+            )
     }
 })
